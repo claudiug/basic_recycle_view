b3c6ef135e6376c86befc3a6edcc14fbca4ef0fa
claudiu <claudiu.garba@gmail.com>
2015-11-28 16:14:14 +0100

    add toolbar

--- app/src/main/java/pro/outbox/claudiug/de/myapplication/ScrollAwareFABBehavior.java
+++ app/src/main/java/pro/outbox/claudiug/de/myapplication/ScrollAwareFABBehavior.java
@@ -1,14 +1,20 @@
 package pro.outbox.claudiug.de.myapplication;
 
+import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
 import android.view.View;
 
 /**
  * Created by claudiug on 11/28/15.
  */
 public class ScrollAwareFABBehavior extends FloatingActionButton.Behavior {
+
+    public ScrollAwareFABBehavior(Context context, AttributeSet attrs) {
+        super();
+    }
 
     @Override
     public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout,
@@ -23,8 +29,7 @@ public class ScrollAwareFABBehavior extends FloatingActionButton.Behavior {
     @Override
     public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child,
                                View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
-        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed,
-                dyUnconsumed);
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
 
         if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
             child.hide();
